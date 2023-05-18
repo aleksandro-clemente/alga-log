220cf28d9782bb6d6e78678831f884f42688551a
Aleksandro Clemente <aleksandro1404@gmail.com>
2023-05-18 20:52:07 -0300

    implementado buscar,adicionar,atualizar,excluir

--- src/main/java/com/algaworks/algalog/service/ClienteService.java
+++ src/main/java/com/algaworks/algalog/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.algaworks.algalog.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,8 @@ import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.dto.ClienteDTO;
 import com.algaworks.algalog.repository.ClienteRepository;
 import com.algaworks.algalog.service.exceptions.EntidadeNaoEncontradaException;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
 
 @Service
 public class ClienteService {
@@ -27,4 +30,30 @@ public class ClienteService {
 				.orElseThrow(()-> new EntidadeNaoEncontradaException("Recurso nao encontrado"));
 		return new ClienteDTO(cliente);
 	}
+	
+	public ClienteDTO adicionar(ClienteDTO dto) {
+		Cliente cliente = new Cliente();
+		copiarDtoParaCliente(cliente, dto);
+		clienteRepository.save(cliente);
+		return new ClienteDTO(cliente);
+	}
+	
+	public ClienteDTO atualizar(Long clienteId,ClienteDTO dto) {
+		Cliente cliente = clienteRepository.findById(clienteId)
+				.orElseThrow(()-> new EntidadeNaoEncontradaException("Recurso nao encontrado"));
+		copiarDtoParaCliente(cliente,dto);
+		clienteRepository.save(cliente);
+		return new ClienteDTO(cliente);
+	}
+	
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+	
+	public void copiarDtoParaCliente(Cliente cliente,ClienteDTO dto) {
+		cliente.setNome(dto.getNome());
+		cliente.setEmail(dto.getEmail());
+		cliente.setFone(dto.getFone());;
+	}
+
 }
